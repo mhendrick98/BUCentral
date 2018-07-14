1af3e66447548cbed16c46c7e51f07bc8815397a
mhendrick98 <mhendric@bu.edu>
2018-07-14 18:28:53 -0400

    First login screen

--- BUCentral/ViewController.swift
+++ BUCentral/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func signUp(_ sender: Any) {
+        Auth.auth().createUser(withEmail: self.usernameInput.text!, password: self.passwordInput.text!, completion: { (user, error) in
+            if user != nil{
+                print("User has signed up!")
+            }
+            if error != nil{
+                print(":(")
+            }
+            })
+    }
+    
+    @IBAction func logIn(_ sender: Any) {
+        Auth.auth().signIn(withEmail: self.usernameInput.text!, password: self.passwordInput.text!, completion: { (user, error) in
+            if user != nil{
+                print("User has logged in!")
+            }
+            if error != nil{
+                print(":(")
+            }
+        })
+    }
 }
 
